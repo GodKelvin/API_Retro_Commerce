8210a2757cd4f4f5650e5e6645cb8ec4972973b9
GodKelvin <kelvin.lehrback@gmail.com>
2023-08-14 17:29:44 -0300

    alterando estrutura do banco de dados

--- src/database/scripts/seed.sql
+++ src/database/scripts/seed.sql
@@ -1,3 +1,7 @@
 -- Inserir dados na tabela "tipos_usuarios"
 INSERT INTO tipos_usuarios (tipo)
 VALUES ('adm'), ('comum');
+
+INSERt INTO consoles (nome, fabricante, geracao, lancamento_jp, lancamento_na, lancamento_pal)
+VALUES ('Mega Drive', 'Sega', 4, '1998-10-29', '1989-08-14', '1990-09-01')
+VALUES ('Super Nintendo Entertainment System', 'Nintendo', 4, '1990-11-21', '1991-08-23', '1993-08-30')
