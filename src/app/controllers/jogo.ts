ddc8463e5701cd1a5f1e20b30f16e7e26190ecb1
GodKelvin <kelvin.lehrback@gmail.com>
2023-08-15 14:14:41 -0300

    implementando busca de jogo por plataforma ou nome

--- src/app/controllers/jogo.ts
+++ src/app/controllers/jogo.ts
@@ -3,19 +3,21 @@ import { Jogo } from "../models/jogo";
 import auth from "../middlewares/auth";
 
 const jogoRouter = Router();
-jogoRouter.get("/:console", auth.checkToken, async(req: Request, res: Response): Promise<any> => {
+jogoRouter.get("/", auth.checkToken, async(req: Request, res: Response): Promise<any> => {
     try{
-        if(!req.params.console) return res.status(400).json({
-            success: false,
-            message: "Informe um console"
-        });
-        let search = await Jogo.getByConsole(req.params.console);
+        if(!req.query.jogo && !req.query.plataforma){
+            return res.status(200).json({
+                success: false,
+                message: "Informe um jogo ou uma plataforma"
+            }); 
+        }
+        let search = await Jogo.search(req.query);
         return res.status(200).json({
             success: true,
             message: search
         });
     }catch(error){
-        console.log(`>> ERROR: Get Jogos ${console}}:\n${error}`);
+        console.log(`>> ERROR: Get Jogos: ${req.params.console}}:\n${error}`);
         return res.status(500).json({
             error: `Erro ao obter lista de jogos. Por favor, tente mais Tarde`}
         );
--- src/app/models/jogo.ts
+++ src/app/models/jogo.ts
@@ -11,4 +11,23 @@ export class Jogo{
                     .select(["jogos.nome", "jogos.desenvolvedora", "jogos.publicadora", 
                     "jogos.lancamento_jp", "jogos.lancamento_na", "jogos.lancamento_pal"]) as IJogo[];
     }
+
+    static async search(query: any): Promise<IJogo[]>{
+
+        const colunas = ["jogos.nome", "jogos.publicadora", "jogos.desenvolvedora", "jogos.lancamento_jp",
+                        "jogos.lancamento_na", "jogos.lancamento_pal", "consoles.nome AS plataforma"]
+
+        let consulta = db("jogos").join("consoles", "jogos.console_id", "consoles.id")
+
+        if(query.plataforma){
+            consulta = consulta.whereRaw("LOWER(consoles.abreviacao) = LOWER(?)", query.plataforma)
+        }
+
+        if(query.jogo){
+            consulta = consulta.where("jogos.nome", "ilike", `%${query.jogo}%`)
+        }
+
+        //@TODO: Buscar via demais parametros?
+        return await consulta.select(colunas);
+    }
 }
