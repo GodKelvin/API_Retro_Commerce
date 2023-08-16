a011249cc2ff15e10d2fcc42110567a80510d30f
Kelvin Lehrback <kelvin.lehrback@gmail.com>
2023-08-16 16:07:49 -0300

    Inserindo model de jogo. Criando rotas de Jogo. Alterando banco de dados. (#9)
    * alterando estrutura do banco de dados
    
    * implementando jogo
    
    * implementando busca de jogo por plataforma ou nome

---
+++ src/app/controllers/jogo.ts
@@ -0,0 +1,27 @@
+import { Request, Response, Router } from "express";
+import { Jogo } from "../models/jogo";
+import auth from "../middlewares/auth";
+
+const jogoRouter = Router();
+jogoRouter.get("/", auth.checkToken, async(req: Request, res: Response): Promise<any> => {
+    try{
+        if(!req.query.jogo && !req.query.plataforma){
+            return res.status(200).json({
+                success: false,
+                message: "Informe um jogo ou uma plataforma"
+            }); 
+        }
+        let search = await Jogo.search(req.query);
+        return res.status(200).json({
+            success: true,
+            message: search
+        });
+    }catch(error){
+        console.log(`>> ERROR: Get Jogos: ${req.params.console}}:\n${error}`);
+        return res.status(500).json({
+            error: `Erro ao obter lista de jogos. Por favor, tente mais Tarde`}
+        );
+    }
+});
+
+export default jogoRouter;
---
+++ src/app/models/jogo.ts
@@ -0,0 +1,33 @@
+import { IJogo } from "../interfaces/jogo";
+import db from "../../database/knexInit";
+
+export class Jogo{
+    constructor(){}
+
+    static async getByConsole(abreviacaoConsole: string): Promise<IJogo[]>{
+        return await db("jogos")
+                    .join("consoles", "jogos.console_id", "consoles.id")
+                    .whereRaw("LOWER(consoles.abreviacao) = ?", abreviacaoConsole.toLocaleLowerCase())
+                    .select(["jogos.nome", "jogos.desenvolvedora", "jogos.publicadora", 
+                    "jogos.lancamento_jp", "jogos.lancamento_na", "jogos.lancamento_pal"]) as IJogo[];
+    }
+
+    static async search(query: any): Promise<IJogo[]>{
+
+        const colunas = ["jogos.nome", "jogos.publicadora", "jogos.desenvolvedora", "jogos.lancamento_jp",
+                        "jogos.lancamento_na", "jogos.lancamento_pal", "consoles.nome AS plataforma"]
+
+        let consulta = db("jogos").join("consoles", "jogos.console_id", "consoles.id")
+
+        if(query.plataforma){
+            consulta = consulta.whereRaw("LOWER(consoles.abreviacao) = LOWER(?)", query.plataforma)
+        }
+
+        if(query.jogo){
+            consulta = consulta.where("jogos.nome", "ilike", `%${query.jogo}%`)
+        }
+
+        //@TODO: Buscar via demais parametros?
+        return await consulta.select(colunas);
+    }
+}
