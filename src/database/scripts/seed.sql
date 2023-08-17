00666dc4e328b4c761fb13283696e4d045250ab6
GodKelvin <kelvin.lehrback@gmail.com>
2023-08-17 20:25:24 -0300

    ajustando seed

--- src/database/scripts/seed.sql
+++ src/database/scripts/seed.sql
@@ -4,8 +4,7 @@ VALUES ('adm'), ('comum');
 
 INSERT INTO consoles (nome, abreviacao, fabricante, geracao, lancamento_jp, lancamento_na, lancamento_pal)
 VALUES  ('Mega Drive', 'Genesis', 'Sega', 4, '1998-10-29', '1989-08-14', '1990-09-01'),
-        ('Super Nintendo Entertainment System', 'SNES', 'Nintendo', 4, '1990-11-21', '1991-08-23', '1993-08-30')
+        ('Super Nintendo Entertainment System', 'SNES', 'Nintendo', 4, '1990-11-21', '1991-08-23', '1993-08-30');
 
 INSERT INTO estados_conservacao (estado)
-VALUES ('Lacrado'), ('Novo'), ('Quase Novo'), ('Ligeiramente Usado'), ('Usado'), ('Muito Usado'), ('Danificado')
-
+VALUES ('Lacrado'), ('Novo'), ('Quase Novo'), ('Ligeiramente Usado'), ('Usado'), ('Muito Usado'), ('Danificado');
